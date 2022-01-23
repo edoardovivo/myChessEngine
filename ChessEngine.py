0069a911b0c2ba0df5244c5fa8295e3e386ed4cc
Edoardo <vivoedoardo@gmail.com>
2022-01-23 11:17:00 +0000

    setting up the game

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,1 +1,23 @@
-import pygame
+"""
+Responsible for storing all the info about the state of a chess game. Also responsible for determining valid moves at the current states and a move log.
+"""
+
+class GameState():
+
+    def __init__(self):
+        # board is a 8x8 2d list. Each element has 2 characters. The first characters is the color, 'b' or 'w'. The second character represent the type of the piece.
+        # '--' represent a square with no pieces on it.
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["--","--","--","--","--","--","--","--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
+        
+
--- ChessMain.py
+++ ChessMain.py
@@ -1,3 +1,28 @@
 """
 Main driver file. Responsible for handling user input and displaying game state.
 """
+
+from myChessEngine import ChessEngine
+import pygame as p
+
+WIDTH = HEIGHT = 512
+DIMENSION = 8
+SQ_SIZE = HEIGHT // DIMENSION
+MAX_FPS = 15
+IMAGES = {}
+
+"""
+Initialize a global dictionary of images. Called exactly once in the main
+"""
+def load_images():
+    pieces = ["bR", "bN", "bB", "bQ", "bK", "bp", "wR", "wN", "wB", "wQ", "wK", "wp"]
+    IMAGES = {piece: p.transform.scale(p.image.load("images/{}.png".format(piece)), (SQ_SIZE, SQ_SIZE) ) for piece in pieces}
+
+
+"""
+Main driver. This will handle user input and updating the graphics
+"""
+def main():
+    p.init()
+    
+
